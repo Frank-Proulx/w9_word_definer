c4a608161ec9e9de8daac79c24a1fbc20bc017e0
Frank-Proulx <frankproulx7@gmail.com>
2021-12-17 09:23:59 -0800

    add .all method to word class after passing test

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -2,3 +2,13 @@ require 'rspec'
 require 'definition'
 require 'word'
 require 'pry'
+
+# describe('Definition') do
+  
+#   describe('.all') do
+#     it('returns an empty array when there are no definitions') do
+
+#     end
+#   end
+
+# end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -1,3 +1,25 @@
 require 'rspec'
 require 'word'
 require 'pry'
+
+describe('Word') do
+  
+  describe('.all') do
+    it('returns an empty array when there are no words') do
+      expect(Word.all).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('saves a word into the @@words hash') do
+      word1 = Word.new({:word => "cats", :id => nil})
+      word1.save
+      word2 = Word.new({:word => "cats", :id => nil})
+      word2.save
+      expect(Word.all.to(eq([word1, word2])))
+    end
+  end
+
+
+  
+end
