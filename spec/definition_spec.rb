fd45ae33a4a99e9307d3a866a97aff4912688905
Frank-Proulx <frankproulx7@gmail.com>
2021-12-17 11:46:23 -0800

    add update method to definition class having passed test

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -17,17 +17,17 @@ describe('Definition') do
 
   describe('#==') do
     it("is the same definition if it has the same attributes as another definition") do
-      definition1 = Definition.new({:word => "cats", :word_id => @word.id, :id => nil})
-      definition2 = Definition.new({:word => "cats", :word_id => @word.id, :id => nil})
+      definition1 = Definition.new({:definition => "definition of a word", :word_id => @word.id, :id => nil})
+      definition2 = Definition.new({:definition => "definition of a word", :word_id => @word.id, :id => nil})
       expect(definition1).to(eq(definition2))
     end
   end
 
   describe('#save') do
     it('saves a definition into the @@definitions hash') do
-      definition1 = Definition.new({:word => "cats", :word_id => @word.id, :id => nil})
+      definition1 = Definition.new({:definition => "definition of a word", :word_id => @word.id, :id => nil})
       definition1.save
-      definition2 = Definition.new({:word => "dogs", :word_id => @word.id, :id => nil})
+      definition2 = Definition.new({:definition => "definition of a different word", :word_id => @word.id, :id => nil})
       definition2.save
       expect(Definition.all).to(eq([definition1, definition2]))
     end
@@ -35,9 +35,9 @@ describe('Definition') do
 
   describe('.clear') do
     it('will clear the class variables "@@definitions" and "@@total_rows" to clear all definitions') do
-      definition1 = Definition.new({:word => "cats", :word_id => @word.id, :id => nil})
+      definition1 = Definition.new({:definition => "definition of a word", :word_id => @word.id, :id => nil})
       definition1.save
-      definition2 = Definition.new({:word => "dogs", :word_id => @word.id, :id => nil})
+      definition2 = Definition.new({:definition => "definition of a different word", :word_id => @word.id, :id => nil})
       definition2.save
       Definition.clear
       expect(Definition.all).to(eq([]))
@@ -46,11 +46,22 @@ describe('Definition') do
 
   describe('.find') do
     it('finds a definition by its id') do
-      definition1 = Definition.new({:word => "cats", :word_id => @word.id, :id => nil})
+      definition1 = Definition.new({:definition => "definition of a word", :word_id => @word.id, :id => nil})
       definition1.save
-      definition2 = Definition.new({:word => "dogs", :word_id => @word.id, :id => nil})
+      definition2 = Definition.new({:definition => "definition of a different word", :word_id => @word.id, :id => nil})
       definition2.save
       expect(Definition.find(definition1.id)).to(eq(definition1))
     end
   end
+
+  describe('#update') do
+    it("updates a definition by id") do
+      definition1 = Definition.new({:definition => "definition of a word", :word_id => @word.id, :id => nil})
+      definition1.save
+      definition1.update("different definition", @word.id)
+      expect(definition1.definition).to(eq("different definition"))
+    end
+  end
+
+
 end
